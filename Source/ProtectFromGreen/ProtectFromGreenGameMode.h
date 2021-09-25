6ee0f8549d4fc92e7465dc9b6b6b1dd78c5079b8
Trevorego <metinegeozdemir@gmail.com>
2021-09-25 21:00:31 +0300

    Initial commit

---
+++ Source/ProtectFromGreen/ProtectFromGreenGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProtectFromGreenGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProtectFromGreenGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProtectFromGreenGameMode();
+};
+
+
+
